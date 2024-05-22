12828e4c593e757b220ffcbace5ff338f48f1baf
irris <160921248+irrisdev@users.noreply.github.com>
2024-05-22 17:19:07 +0100

    Change switch statement case

--- handlers/create_url.go
+++ handlers/create_url.go
@@ -1,7 +1,6 @@
 package handlers
 
 import (
-	"fmt"
 	"net/http"
 )
 
@@ -9,7 +8,6 @@ func CreateURL(w http.ResponseWriter, r *http.Request) {
 
 	switch r.Method {
 	case http.MethodPost:
-		fmt.Fprint(w, "Creating URL")
 		break
 	default:
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
